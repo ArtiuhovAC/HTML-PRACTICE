1bf1a44cce076b30c4ccf904924aaad890a35646
Your Name <you@example.com>
2024-06-23 20:19:03 +0300

    JS Упражнение 3

--- JS_PRACTICE/main.js
+++ JS_PRACTICE/main.js
@@ -1,1 +1,13 @@
-alert("Добро пожаловать!");
+const Name = prompt("Введите сюда своё имя", "Введите имя");
+alert(`Ваше имя: ${Name}`);
+const restart = confirm("Начать заново?");
+if (restart) {
+    const response = confirm("Не надоело?");
+    if (response) {
+        alert("Да, это интересно!");
+    } else {
+        alert("Ну и правильно!");
+    }
+} else {
+    alert("До свидания!");
+}
